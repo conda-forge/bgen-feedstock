295d59debfb579819312983dd79ce5edc728909b
Danilo Horta <danilo.horta@pm.me>
2020-08-23 19:37:41 +0100

    dynamic library only

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,5 +8,6 @@ export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 sed -ie 's/-Werror//g' CMakeLists.txt
 mkdir build && cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX $SRC_DIR
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX $SRC_DIR
 make -j$CPU_COUNT && make test && make install
